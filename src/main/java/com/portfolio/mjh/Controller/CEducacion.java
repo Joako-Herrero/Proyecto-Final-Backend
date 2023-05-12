699d35e52cafbf0fd4eb3cf9f9049121a35a466e
Joako-Herrero <joaquinherrero13@gmail.com>
2023-05-11 23:25:30 -0300

    agregando settings.xml porque lo borre sin querer

--- src/main/java/com/portfolio/mjh/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mjh/Controller/CEducacion.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/educacion")
 @CrossOrigin(origins = "https://frontend-mjh.web.app")
+
 public class CEducacion {
 
     @Autowired
